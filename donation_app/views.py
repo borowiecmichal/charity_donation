94eb7eb22941e403762a26eb46995bc077191e0d
Michal <michalborowiec@op.pl>
2021-03-24 12:48:21 +0100

    display organization at main page

--- donation_app/views.py
+++ donation_app/views.py
@@ -9,7 +9,10 @@ class LandingPageView(View):
     def get(self, request):
         ctx = {
             'num_of_given_bags': Donation.objects.aggregate(Sum('quantity'))['quantity__sum'],
-            'num_of_supported_orgs': Donation.objects.all().distinct('institution').count()
+            'num_of_supported_orgs': Donation.objects.all().distinct('institution').count(),
+            'foundations': Institution.objects.filter(type=1),
+            'ngos': Institution.objects.filter(type=2),
+            'locals': Institution.objects.filter(type=3),
         }
         return render(request, 'index.html', ctx)
 
